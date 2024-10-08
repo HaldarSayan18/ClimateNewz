ba65a75ad9b20f08beea6040c667a13d3decd2d3
Sayan Haldar <125077615+HaldarSayan18@users.noreply.github.com>
2024-10-08 13:01:44 +0000

    homepage modified

--- climate-newz/src/App.js
+++ climate-newz/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './assets/styles/App.css';
 import HomePage from './components/HomePage';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
--- climate-newz/src/components/HomePage.jsx
+++ climate-newz/src/components/HomePage.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 import '../assets/styles/HomePage.css'
 import axios from 'axios'
+// import { ToastContainer, toast } from 'react-toastify';
+
+const api = {
+    key: "5dfa8368c3accc79bbf9f5d72591315c",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
 
 const HomePage = () => {
     const [city, setCity] = useState('');
+    const [weather, setWeather] = useState();
     const fetchWeather = async () => {
         try {
-            const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'45c015afdb2f6a20f47f7f18733c6e92'}`)
+            // const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'45c015afdb2f6a20f47f7f18733c6e92'}`)
+            const res = await axios.get(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+            setWeather(res)
             console.log(res);
-            
+
         } catch (error) {
-            console.log("Error occured\n ",error);
-            alert("Error occured\n ",error);
+            console.log("Error occured\n ", error);
+            // alert("Error occured\n ", error);
         }
     }
     const handleCity = (event) => {
@@ -19,16 +28,31 @@ const HomePage = () => {
     }
     const handleClick = () => {
         fetchWeather()
+        // weather ?
+        //     toast.info("City fetched !", {
+        //         position: "top-center"
+        //     })
+        //     :
+        //     toast.error("Enter city name correctly", {
+        //         position: "top-center"
+        //     })
     }
     return (
         <>
             <div className='home-container'>
+                {/* <ToastContainer /> */}
                 <div className='home-sub-container'>
                     <input type='text' placeholder='Enter City Name' value={city} onChange={handleCity} />
                     <button onClick={handleClick} >Fetch Weather</button>
                 </div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officia tempora, consectetur perferendis at recusandae quia totam facilis fugiat magni atque dignissimos rem deleniti laborum illo veritatis unde ipsum! Culpa!
-                Optio exercitationem ullam iure illum neque sunt aut voluptatem natus perferendis eveniet quae expedita harum tenetur, iste omnis earum labore nisi dolor sit quam saepe dolore et sapiente. Vitae, placeat?
+                {weather &&
+                    <div className='weather-info'>
+                        <h3>Coordinates:</h3>
+                            <p>latitude - {weather.data.coord.lat}</p>
+                            <p>longitude - {weather.data.coord.lon}</p>
+                        <h3>Wind Speed: {weather.data.wind.speed}</h3>
+                    </div>
+                }
             </div>
         </>
     )
