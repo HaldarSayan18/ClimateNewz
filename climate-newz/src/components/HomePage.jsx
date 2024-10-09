b83368108e7f5b88c291dcabb5c5049abd73b5ea
Sayan Haldar <125077615+HaldarSayan18@users.noreply.github.com>
2024-10-09 07:01:57 +0000

    basic project & functionalities done

--- climate-newz/src/components/HomePage.jsx
+++ climate-newz/src/components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import '../assets/styles/HomePage.css'
 import axios from 'axios'
+import { FaCaretRight } from "react-icons/fa";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 // import { ToastContainer, toast } from 'react-toastify';
 
 const api = {
@@ -11,6 +14,7 @@ const api = {
 const HomePage = () => {
     const [city, setCity] = useState('');
     const [weather, setWeather] = useState();
+    const [show, setShow] = useState(false)
     const fetchWeather = async () => {
         try {
             // const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'45c015afdb2f6a20f47f7f18733c6e92'}`)
@@ -37,6 +41,9 @@ const HomePage = () => {
         //         position: "top-center"
         //     })
     }
+    const handleToggle = () => {
+        setShow(!show)
+    }
     return (
         <>
             <div className='home-container'>
@@ -47,10 +54,27 @@ const HomePage = () => {
                 </div>
                 {weather &&
                     <div className='weather-info'>
-                        <h3>Coordinates:</h3>
-                            <p>latitude - {weather.data.coord.lat}</p>
-                            <p>longitude - {weather.data.coord.lon}</p>
-                        <h3>Wind Speed: {weather.data.wind.speed}</h3>
+                        {/* <Dropdown as={ButtonGroup} style={{ display: "flex" }} show={show} onToggle={handleToggle} >
+                            {/* <Button variant="success">Split Button</Button> 
+                            <div className='h3'>Coordinates:</div>
+
+                            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" style={{marginLeft:"15px", background:"transparent", border:"none"}}><FaCaretRight /></Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item style={{ textDecoration: "none", color:"black" }}>latitude - {weather.data.coord.lat}</Dropdown.Item>
+                                <br/>
+                                <Dropdown.Item style={{ textDecoration: "none", color:"black" }}>longitude - {weather.data.coord.lon}</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown> */}
+                        <div className='h3'>Coordinates:</div>
+                        <p>latitude - {weather.data.coord.lat}</p>
+                        <p>longitude - {weather.data.coord.lon}</p>
+                        <div className='h3'>Tempareture: {weather.data.main.temp}</div>
+                        <p>Maximum - {weather.data.main.temp_max}</p>
+                        <p>Minimum - {weather.data.main.temp_min}</p>
+                        <div className='h3'>Feels like: {weather.data.main.feels_like}</div>
+                        <div className='h3'>Wind Speed: {weather.data.wind.speed}</div>
+                        {/* <div className='h3'>Wind Speed: {weather.data.name}</div> */}
                     </div>
                 }
             </div>
